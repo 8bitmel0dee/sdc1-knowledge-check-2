f65c50b081cb4ede2ecb62a9160073890c5e8a36
Melody Miller <8bitmel0dee@gmail.com>
2023-12-23 14:41:52 -0500

    I did it! :)

---
+++ KnowledgeCheck2/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck2
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Price: {Price}, Quantity: {Quantity}, Description: {Description}";
+        }
+
+    }
+
+    public class CatFood : Product
+    {
+        public string Flavor { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Flavor: {Flavor}";
+        }
+    }
+}
---
+++ KnowledgeCheck2/Program.cs
@@ -0,0 +1,44 @@
+﻿using KnowledgeCheck2;
+
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<Product>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+
+    var CatFood = new CatFood();
+
+    Console.WriteLine("Enter the name of the cat food: ");
+    CatFood.Name = Console.ReadLine();
+
+    Console.WriteLine("Enter the price of the cat food: ");
+    CatFood.Price = decimal.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter the quantity of the cat food: ");
+    CatFood.Quantity = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter the description of the cat food: ");
+    CatFood.Description = Console.ReadLine();
+
+    Console.WriteLine("Enter the cat food's flavor: ");
+    CatFood.Flavor = Console.ReadLine();
+
+    recordList.Add(CatFood);
+
+    // Print out the list of records using Console.WriteLine()
+
+    foreach (var record in recordList)
+
+    {
+        Console.WriteLine(record.ToString());
+
+        Console.WriteLine();
+    }
+}
+
+
+
+
